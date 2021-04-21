3caeea86084af43cc76b3703b0b818e4175440e9
decodervarun <82942125+decodervarun@users.noreply.github.com>
2021-04-21 11:19:04 +0100

    Update login.php

--- login.php
+++ login.php
@@ -1,12 +1,4 @@
 <?php
-/*
- * @ https://EasyToYou.eu - IonCube v10 Decoder Online
- * @ PHP 5.6
- * @ Decoder version: 1.0.4
- * @ Release: 02/06/2020
- *
- * @ ZendGuard Decoder PHP 5.6
- */
 
 if (!defined("WHMCS")) {
     header("Location: clientarea.php");
