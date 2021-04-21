e3e09f48752b5c0ac426a48563aff880cc8cbfe7
decodervarun <82942125+decodervarun@users.noreply.github.com>
2021-04-21 11:22:30 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,4 @@
 <?php
-/*
- * @ https://EasyToYou.eu - IonCube v10 Decoder Online
- * @ PHP 5.6
- * @ Decoder version: 1.0.4
- * @ Release: 02/06/2020
- *
- * @ ZendGuard Decoder PHP 5.6
- */
 
 define("CLIENTAREA", true);
 require_once __DIR__ . "/init.php";
