b5021d580dad5c0f3c3c828bb89ca944949e0e55
decodervarun <82942125+decodervarun@users.noreply.github.com>
2021-04-21 11:22:53 +0100

    Update link.php

--- link.php
+++ link.php
@@ -1,12 +1,4 @@
 <?php
-/*
- * @ https://EasyToYou.eu - IonCube v10 Decoder Online
- * @ PHP 5.6
- * @ Decoder version: 1.0.4
- * @ Release: 02/06/2020
- *
- * @ ZendGuard Decoder PHP 5.6
- */
 
 require "init.php";
 $id = (int) $whmcs->get_req_var("id");
